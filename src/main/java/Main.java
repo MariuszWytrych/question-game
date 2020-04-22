7561fd60ac6781cecba753f072dd768c70085946
MariuszWy <mariusz.wytrych@gmail.com>
2020-04-22 22:08:18 +0200

    Add type silent mode and read answer from file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 import java.io.*;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.logging.SocketHandler;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -39,55 +41,49 @@ public class Main {
         System.out.println("WItaj w grze słownej. Za chwilę otrzymasz 5 pytań, na każde z nich idziel odowiedzi T lub N.\n" +
                 "Gdzie T oznacza Tak, a N oznacza Nie");
         System.out.println("Zaczynamy!");
+
+        System.out.println("Wybierz tryb odpowiedzi\n 1 - tryb cichy\n 2 - tryb głośny");
+
+        int optionMode;
         Scanner scanner = new Scanner(System.in);
-        String answer;
+        optionMode = scanner.nextInt();
+        typeMode(optionMode);
 
 
-        BufferedReader br = new BufferedReader(new FileReader("question.txt"));
-        String line;
+        Scanner scannerFileQuestion = new Scanner(new File("question.txt"));
+        BufferedReader bRAnswer = new BufferedReader((new FileReader("answers.txt")));
+        String lineFromFileQuestion;
+        String lineFromFileAnswer;
+
         try {
-            while ((line = br.readLine()) != null) {
-                String[] columnForQuestion = line.split(";");
-                System.out.println(columnForQuestion[1]);
-                int idQuestion = Integer.parseInt(columnForQuestion[0]);
-                int idQuestionForT = Integer.parseInt(columnForQuestion[2]);
-                int idQuestionForN = Integer.parseInt(columnForQuestion[3]);
-                answer = scanner.nextLine().toUpperCase();
-                switch (answer) {
-                    case "T":
-                        System.out.println(columnForQuestion[4]);
-                        if (idQuestion == idQuestionForT) {
-                            System.out.println(columnForQuestion[1]);
-                            answer = scanner.nextLine().toUpperCase();
-                            while (answer.equalsIgnoreCase("T")){
-                                System.out.println(columnForQuestion[1]);
-                                answer = scanner.nextLine().toUpperCase();
-                            }
-
-                        }
-                        break;
-                    case "N":
-                        System.out.println(columnForQuestion[5]);
-                        if (idQuestion == idQuestionForN) {
-                                System.out.println(columnForQuestion[1]);
-                                answer = scanner.nextLine().toUpperCase();
-                                while (answer.equalsIgnoreCase("N")){
-                                    System.out.println(columnForQuestion[1]);
-                                    answer = scanner.nextLine().toUpperCase();
-                                }
-                        }
-                        break;
-                    default:
-                        System.out.println("no match");
+            while ((lineFromFileAnswer = bRAnswer.readLine()) != null) {
+                String[] answerFromFile = lineFromFileAnswer.split("");
+                for (int i = 0; i < answerFromFile.length; i++) {
+                    lineFromFileQuestion = scannerFileQuestion.nextLine();
+                    String[] columnForQuestion = lineFromFileQuestion.split(";");
+                    String question = columnForQuestion[1];
+                    if (typeMode(optionMode) == false) {
+                        System.out.println(question);
+                        System.out.println(answerFromFile[i]);
+                    }
 
                 }
+                if (scannerFileQuestion.hasNext()) {
+                    System.out.println("Failed");
+                } else {
+                    System.out.println("Passed");
+                }
             }
-            br.close();
-        } catch (Exception e) {
-            System.err.println("Wystapil blad przy wczytywaniu danych");
-            e.printStackTrace();
+            bRAnswer.close();
+            scannerFileQuestion.close();
+
+        } catch (Exception fail) {
+            System.err.println("Wystąpił błąd wczytywania pliku.");
+            fail.printStackTrace();
         }
-        System.out.println("Passed");
+
+
+//        gameOnConsole(scanner);
 
 
 //        String answer;
@@ -109,4 +105,63 @@ public class Main {
 //
 //
     }
+
+    private static void gameOnConsole(Scanner scanner) throws FileNotFoundException {
+        String answer;
+        BufferedReader br = new BufferedReader(new FileReader("question.txt"));
+        String line;
+        try {
+            while ((line = br.readLine()) != null) {
+                String[] columnForQuestion = line.split(";");
+                System.out.println(columnForQuestion[1]);
+                int idQuestion = Integer.parseInt(columnForQuestion[0]);
+                int idQuestionForT = Integer.parseInt(columnForQuestion[2]);
+                int idQuestionForN = Integer.parseInt(columnForQuestion[3]);
+                answer = scanner.nextLine().toUpperCase();
+                switch (answer) {
+                    case "T":
+                        System.out.println(columnForQuestion[4]);
+                        if (idQuestion == idQuestionForT) {
+                            System.out.println(columnForQuestion[1]);
+                            answer = scanner.nextLine().toUpperCase();
+                            while (answer.equalsIgnoreCase("T")) {
+                                System.out.println(columnForQuestion[1]);
+                                answer = scanner.nextLine().toUpperCase();
+                            }
+
+                        }
+                        break;
+                    case "N":
+                        System.out.println(columnForQuestion[5]);
+                        if (idQuestion == idQuestionForN) {
+                            System.out.println(columnForQuestion[1]);
+                            answer = scanner.nextLine().toUpperCase();
+                            while (answer.equalsIgnoreCase("N")) {
+                                System.out.println(columnForQuestion[1]);
+                                answer = scanner.nextLine().toUpperCase();
+                            }
+                        }
+                        break;
+                    default:
+                        System.out.println("no match");
+
+                }
+            }
+            br.close();
+        } catch (Exception e) {
+            System.err.println("Wystapil blad przy wczytywaniu danych");
+            e.printStackTrace();
+        }
+        System.out.println("Koniec gry!");
+    }
+
+    public static boolean typeMode(int optionMode) {
+        if (optionMode == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
 }
